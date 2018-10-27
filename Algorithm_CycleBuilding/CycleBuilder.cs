436dfb0fb7b9bd4434cae2270ffd9e3fafcd13b9
rov3rPL <hieronim1981@gmail.com>
2018-10-27 14:40:56 +0200

    Performance test of DFS algorithm
    init commit

---
+++ Algorithm_CycleBuilding/CycleBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Algorithm_CycleBuilding
+{
+    class CycleBuilder
+    {
+        private int StartSpanInDays;
+        private int NumberOfBlocks;
+        private int DaySpan;
+        private int TimeOfBlockBuilding;
+        private int ProbabilityOfSuccessBlockBuilding;
+
+        internal int NumberOfBlockBuildingTries { get; private set; }
+
+        internal CycleBuilder(int startSpanInDays, int numberOfBlocks, int daySpan, int timeOfBlockBuilding, int probabilityOfSuccessBlockBuilding)
+        {
+            StartSpanInDays = startSpanInDays;
+            NumberOfBlocks = numberOfBlocks;
+            DaySpan = daySpan;
+            TimeOfBlockBuilding = timeOfBlockBuilding;
+            ProbabilityOfSuccessBlockBuilding = probabilityOfSuccessBlockBuilding;
+
+            NumberOfBlockBuildingTries = 0;
+
+            TimeSpan start = DateTime.Now.TimeOfDay;
+            var success = Start(NumberOfBlocks);
+            TimeSpan end = DateTime.Now.TimeOfDay;
+            var diff = (end - start).TotalMilliseconds;
+            Console.WriteLine($"[Success] {success} [Time] {diff} ms, Block building tries = {NumberOfBlockBuildingTries}");
+
+        }
+
+        private bool Start(int numberOfBlocks)
+        {
+            for(int i=0; i<StartSpanInDays; ++i)
+            {
+                var isSuccess = FindSolution(numberOfBlocks);
+                if (isSuccess)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool FindSolution(int numberOfBlocks)
+        {
+            if (numberOfBlocks == 0)
+                return true;
+
+            for(int i=0; i<DaySpan; ++i)
+            {
+                var isBlock = TryToBuildBlock();
+                if (isBlock)
+                    return FindSolution(numberOfBlocks - 1);
+            }
+            return false;
+        }
+ 
+        private bool TryToBuildBlock()
+        {
+            System.Threading.Thread.Sleep(TimeOfBlockBuilding);
+            ++NumberOfBlockBuildingTries;
+            var r = new System.Random();
+            var rand = r.Next(100);
+            return rand < ProbabilityOfSuccessBlockBuilding;
+        }
+
+    }
+}
---
+++ Algorithm_CycleBuilding/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CycleBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cycleBuilder = new CycleBuilder(6, 13, 5, 30, 2); //576 ms
+
+            //var cycleBuilder = new CycleBuilder(7, 10, 2, 20, 33); //480 ms
+
+            //var cycleBuilder = new CycleBuilder(14, 10, 2, 20, 10); //660-695 ms
+
+            //var cycleBuilder = new CycleBuilder(14, 10, 2, 40, 10); //1352 ms
+
+            var cycleBuilder = new CycleBuilder(14, 12, 5, 15, 20); //False [Time] 3584,9881 ms
+
+
+            Console.ReadLine();
+        }
+    }
+}
